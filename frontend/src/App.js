9c0dabe0b2be6766fb27732bdda31482fcf8f9e4
Slawomir Serafin <margolek555@gmail.com>
2021-10-29 20:35:34 +0200

    Content using react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Button, Container, Card, Row, Col } 
+from 'react-bootstrap';
+import {AiFillCheckCircle} from 'react-icons/ai';
+import {MdUpdate} from 'react-icons/md';
+import {AiFillDelete} from 'react-icons/ai';
+import {BsArrowDownCircleFill} from 'react-icons/bs';
+import { IconContext } from "react-icons";
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
 
 function App() {
-  return (
-    <h2>Basic setup</h2>
+
+  const [showDescription,setShowDescription] = useState(true);
+
+  return ( 
+  <Container>
+    <Card>
+      <Card.Title>
+        <h1>To Do List</h1>
+      </Card.Title>
+      <div className="underline"></div>
+      <Card.Body>
+        <div className="task-items">
+            <Row>
+              <Col className="task-text">
+                <h5>In here I will map data using API</h5>
+                {showDescription ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+              </Col>
+              <Col  className="icons">
+                <IconContext.Provider value={{ className: "done" }}>
+                  <AiFillCheckCircle onClick={() => console.log("do nothing done")}/>
+                </IconContext.Provider>
+                <IconContext.Provider value={{ className: "update" }}>
+                  <MdUpdate onClick={() => console.log("do nothing update")}/>
+                </IconContext.Provider>
+                <IconContext.Provider value={{ className: "delete" }}>
+                  <AiFillDelete onClick={() => console.log("do nothing delete")}/>
+                </IconContext.Provider>
+              </Col>
+              <div>text description will be here</div>
+              <hr/>
+            </Row>
+            
+        </div>
+      </Card.Body>
+    </Card>
+  </Container>
   );
 }
 
